6f6182d236562efee0a29f19fc52bdedd054cc4b
Fair Pony <lljllj122@gmail.com>
2020-04-04 18:23:21 +0800

    Added unit tests for opstack and local vars

---
+++ ch04/rtda/local_vars_test.go
@@ -0,0 +1,43 @@
+package rtda
+
+import (
+	"jvmgo/ch04/test/assert"
+	"math/rand"
+	"testing"
+)
+
+func TestInt(t *testing.T) {
+	localVars := newLocalVars(10)
+	for i := range localVars {
+		v := rand.Int31()
+		localVars.SetInt(uint(i), v)
+		assert.Equal(t, v, localVars.GetInt(uint(i)), "")
+	}
+}
+
+func TestFloat(t *testing.T) {
+	localVars := newLocalVars(10)
+	for i := range localVars {
+		v := rand.Float32()
+		localVars.SetFloat(uint(i), v)
+		assert.Equal(t, v, localVars.GetFloat(uint(i)), "")
+	}
+}
+
+func TestLong(t *testing.T) {
+	localVars := newLocalVars(10)
+	for i := 0; i < 5; i++ {
+		v := rand.Int63()
+		localVars.SetLong(uint(i*2), v)
+		assert.Equal(t, v, localVars.GetLong(uint(i*2)), "")
+	}
+}
+
+func TestDouble(t *testing.T) {
+	localVars := newLocalVars(10)
+	for i := 0; i < 5; i++ {
+		v := rand.Float64()
+		localVars.SetDouble(uint(i*2), v)
+		assert.Equal(t, v, localVars.GetDouble(uint(i*2)), "")
+	}
+}
---
+++ ch04/test/assert/assert.go
@@ -0,0 +1,16 @@
+package assert
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Equal(t *testing.T, a interface{}, b interface{}, message string) {
+	if a == b {
+		return
+	}
+	if len(message) == 0 {
+		message = fmt.Sprintf("%v != %v", a, b)
+	}
+	t.Fatal(message)
+}
